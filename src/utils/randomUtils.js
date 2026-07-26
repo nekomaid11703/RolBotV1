ab01e3e99e12630b7c45afd45df6e6e3b6a1df48
nekomaid <nekomaid7564@gmail.com>
2026-07-25 20:23:26 -0500

    test commit

--- src/utils/randomUtils.js
+++ src/utils/randomUtils.js
@@ -1,13 +1,25 @@
 const { randomInt: secureRandomInt } = require("crypto");
 
+/**
+ *
+ * @param min
+ * @param max
+ */
 function randomInt(min, max) {
   return secureRandomInt(min, max);
 }
 
+/**
+ *
+ */
 function randomFloat() {
   return secureRandomInt(0, 1000001) / 1000000;
 }
 
+/**
+ *
+ * @param length
+ */
 function randomId(length = 4) {
   const min = Math.pow(36, length - 1);
   const max = Math.pow(36, length);
